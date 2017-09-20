e61e26215612a9c8aa131b25e5ac43f7cbb3435c
BowenWu <bowen.wu@163.com>
2017-09-20 15:40:04 +0800

    Fix util

--- HW1/util.py
+++ HW1/util.py
@@ -7,7 +7,6 @@ import numpy as np
 def toMatrix(img):
     data = img.getdata()
     data = np.matrix(data, dtype=np.uint8)
-    # 这样做其实不符合numpy的风格
     # http://scikit-image.org/docs/dev/user_guide/numpy_images.html#numpy-indexing
     # Be careful: in NumPy indexing, the first dimension 
     # (camera.shape[0]) corresponds to rows, while the second (camera.shape[1]) corresponds to columns, with the origin (camera[0, 0]) on the top-left corner. 
@@ -16,10 +15,5 @@ def toMatrix(img):
     return data
 # matrix : numpy.matrix
 def toImage(matrix):
-    # 这很奇怪 我发现如果直接转换回image 那么长宽会刚好反过来 可能是库里的bug
-    # It is weird that this function will exchange image's width and height if 
-    # use it directly. It may a bug of PIL
-    #w, h = matrix.shape
-    #matrix = np.reshape(matrix, (h, w))
     img = Image.fromarray(matrix)
     return img

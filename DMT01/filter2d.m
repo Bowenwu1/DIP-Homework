08fdfeb8088339369e31d7d2beff8c5f923036d3
BowenWu <bowen.wu@163.com>
2017-11-29 20:30:38 +0800

    add DMT01

---
+++ DMT01/filter2d.m
@@ -0,0 +1,58 @@
+function [ output_img ] = filter2d(img, filter, whetherDraw)
+% FILTER2D
+% filter : a matrix
+% input_img_name : path of image file
+% whetherDraw : whether to draw the origin and after image
+if nargin == 2
+    whetherDraw = true;
+end
+
+img = double(img);
+[h, w] = size(img);
+output_img = zeros(h, w);
+[f_h, f_w] = size(filter);
+halfFilterLength_h = floor(f_h / 2);
+halfFilterLength_w = floor(f_w / 2);
+filter_center = [halfFilterLength_h + 1, halfFilterLength_w + 1];
+% calculate total weight, in order to make total weight = 1
+
+% filter
+for i = 1 : h
+    for j = 1 : w
+        % center is (i, j)
+        sum = 0;
+        total_weight = 0;
+        for m = -1 * halfFilterLength_h : halfFilterLength_h
+            for n = -1 * halfFilterLength_w : halfFilterLength_w
+                if ((i + m) < 1 || (i + m) > h)
+                    % over border 1
+                    sum = sum + 0;
+                elseif ((j + n) < 1 || (j + n) > w)
+                    % over border 2
+                    sum = sum + 0;
+                else
+                    total_weight = total_weight + filter(filter_center(1) + m, filter_center(2) + n);
+                    sum = sum + img(i + m, j + n) * filter(filter_center(1) + m, filter_center(2) + n);
+                end
+            end
+        end
+        sum = sum / total_weight;
+        output_img(i, j) = sum;
+    end
+end
+
+output_img = uint8(output_img);
+img = uint8(img);
+
+if true == whetherDraw
+    subplot(2, 1, 1);
+    imshow(img);
+    title('origin');
+
+    subplot(2, 1, 2);
+    imshow(output_img);
+    title('after filter');
+end
+
+end
+
---
+++ DMT01/submission.m
@@ -0,0 +1,172 @@
+% Read the image
+img = imread('input.jpg');
+
+% Translate into gray image
+gray_img = rgb2gray(img);
+
+% Filter for noise
+gray_img = filter2d(gray_img, ones(3,3), false);
+
+% High-boost filter
+k = 5;
+high_boost = [-k -k -k;-k 9+8*k -k;-k -k -k];
+gray_img = filter2d(gray_img, high_boost, false);
+
+% Filter for pcik up edge through Lap
+% filter = [-1 -1 -1;-1 9 -1;-1 -1 -1];
+filter = [-1 -2 -1;0 1 0;1 2 1];
+edge_img = filter2d(gray_img, filter, false) - gray_img;
+
+
+% Translate the image into 2-value image
+[w, h] = size(edge_img);
+threshold = 30;
+for m = 1 : w
+    for n = 1 : h
+        if (edge_img(m, n) < threshold)
+            edge_img(m, n) = 0;
+        else
+            edge_img(m, n) = 255;
+        end
+    end
+end
+
+% imshow(edge_img);
+
+% Hough Transform
+% Line form : r = x * cos(theta) + y * sin(theta)
+% Theta : -pi - pi(not include)
+% r : sqrt(w^2 + h^2)
+% theta_start = -pi;
+% theta_level = 500;
+% theta_length = abs(theta_start) / theta_level;
+% hough_space = zeros(theta_level * 2, ceil(sqrt((w + 1)^2 + (h+1)^2)));
+% % Voting for Hough Space
+% for x = 1 : w
+%     for y = 1 : h
+%         if edge_img(x, y) == 255
+%             % find edge
+%             for i = 1 : theta_level * 2
+%                 degree = theta_start + (i - 1) * theta_length;
+%                 r = x * cos(degree) + y * sin(degree);
+%                 r = abs(r);
+%                 r = ceil(r);
+%                 hough_space(i, r) = hough_space(i, r) + 1;
+%             end
+%         end
+%     end
+% end
+% 
+% [hough_space_h, hough_space_w] = size(hough_space);
+% four_line = cell(4);
+% for m = 1 : 4
+%     [c, r] = find(hough_space == max(max(hough_space)));
+%     four_line{m} = [c, r];
+%     hough_space(c, r) = eps;
+% end
+% 
+% image_four_line = zeros(w, h);
+% % Draw four lines
+% for m = 1 : 4
+%     for x = 1 : w
+%         for y = 1 : h
+%             degree = theta_start + (four_line{m}(1) - 1) * theta_length;
+%             r = four_line{m}(2);
+%             r_temp = x * cos(degree) + y * sin(degree);
+%             if ceil(abs(r_temp)) == r
+%                 image_four_line(x, y) = 255;
+%             end
+%         end
+%     end
+% end
+% 
+% image_four_line = uint8(image_four_line);
+% imshow(image_four_line);
+
+[H,Theta,Rho] = hough(edge_img);
+% subplot(222), imshow(H,[],'XData',Theta,'YData',Rho,'InitialMagnification','fit'),...
+%     title('rho\_theta space and peaks');
+% xlabel('\theta'), ylabel('\rho');
+% axis on, axis normal, hold on;
+% label the top 5 intersections
+P  = houghpeaks(H,4,'threshold',ceil(0.4*max(H(:))));
+% x = Theta(P(:,2)); 
+% y = Rho(P(:,1));
+% plot(x,y,'*','color','r');
+
+lines = houghlines(edge_img,Theta,Rho,P,'FillGap',20,'MinLength',200);
+
+% merge into 4 lines
+%
+%
+%
+length(lines)
+
+figure, imshow(img), hold on
+for k = 1:length(lines)
+ xy = [lines(k).point1; lines(k).point2];
+ plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','g');
+ 
+ % plot(xy(1,1), xy(1,2), 'x', 'LineWidth', 2, 'Color', 'yellow');
+ % plot(xy(2,1), xy(2,2), 'x', 'LineWidth', 2, 'Color', 'red');
+end
+% Find Four Points
+endPoints = cell(4);
+pointIndex = 1;
+for m = 1 : length(lines)
+    for n = m + 1 : length(lines)
+        xy_m = [lines(m).point1; lines(m).point2];
+        xy_n = [lines(n).point1; lines(n).point2];
+        k_m = (xy_m(2, 2) - xy_m(1, 2)) / (xy_m(2, 1) - xy_m(1, 1));
+        k_n = (xy_n(2, 2) - xy_n(1, 2)) / (xy_n(2, 1) - xy_n(1, 1));
+        if k_m * k_n < 0
+            [x, y] = findEndPoint(xy_m(1, :), xy_m(2, :), xy_n(1, :), xy_n(2, :));
+            endPoints{pointIndex} = [x; y];
+            pointIndex = pointIndex + 1;
+        end
+    end
+end
+
+
+for k = 1 : pointIndex - 1
+    xy = endPoints{k};
+    plot(xy(1), xy(2), 'x', 'LineWidth', 2, 'Color', 'yellow');
+end
+hold off
+% Tou Shi Bian Huan
+target_width = 210 * 2;
+target_height = 297 * 2;
+target_img = zeros(target_width, target_height, 3);
+% Src_Image
+src_points = zeros(4, 2);
+for i = 1 : 4
+    src_points(i, :) = endPoints{i}(:);
+end
+destination_points = [1 1;1 target_height;target_width 1; target_width target_height]
+TForm = cp2tform(src_points, destination_points, 'projective');
+
+% for m = 1 : target_width
+%     for n = 1 : target_height
+%         temp = tforminv(TForm, m, n);
+%         for k = 1 : 3
+%             if round(temp(1)) <= w && round(temp(2)) <= h && temp(1) >= 1 && temp(2) >= 1
+%                 img(round(temp(1)), round(temp(2)), k);
+%                 target_img(m, n, k) = img(round(temp(1)), round(temp(2)), k);
+%             end
+%         end
+%     end
+% end
+[target_img, xdata, ydata] = imtransform(img, TForm);
+% figure
+% imshow(target_img);
+% hold on
+% % pick up result
+% for i = 1 : 4
+%     point = tformfwd(TForm, [src_points(i, 1) src_points(i, 2)]);
+%     plot(point(1) - xdata(1), point(2) - ydata(1), 'x', 'LineWidth', 2, 'Color', 'yellow');
+% end
+
+target_img = imcrop(target_img, [0 - xdata(1) 0 - ydata(1) target_width target_height]);
+target_img = imresize(target_img, 2);
+figure, hold on
+imshow(target_img);

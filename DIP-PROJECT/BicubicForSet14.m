4c2380ceffe7fa5aeffeab82b35a7a6826847baa
吴博文 <wubowen@wubowendeMBP.lan>
2017-12-23 01:25:26 +0800

    finish test script for Set14

---
+++ DIP-PROJECT/BicubicForSet14.m
@@ -0,0 +1,21 @@
+% Test for Set14 using bicubic method
+clear;
+images = dir('/Users/wubowen/Documents/DIP-Homework/DIP-PROJECT/Set14/*.bmp');
+for i = 1 : length(images)
+    origin_image = imread(images(i).name);
+    [origin_h, origin_w, dimension] = size(origin_image);
+    small_image = zeros(floor(origin_h / 3), floor(origin_w / 3), dimension);
+    big_image = zeros(origin_h, origin_w, dimension);
+    for m = 1 : dimension
+        small_image(:, :, m) = bicubic(origin_image(:, :, m), floor(origin_h / 3), floor(origin_w / 3));
+        big_image(:, :, m) = bicubic(small_image(:, :, m), origin_h, origin_w);
+    end
+    small_image = uint8(small_image);
+    big_image = uint8(big_image);
+    if (3 == dimension)
+        origin_image = rgb2ycbcr(origin_image);
+        big_image = rgb2ycbcr(big_image);
+    end
+    fprintf(strcat(images(i).name, ' : (', num2str(psnr(origin_image(:, :, 1), big_image(:, :, 1))), ', ', ...
+                num2str(SSIM(origin_image(:, :, 1), big_image(:, :, 1))), ')\n'));
+end
--- DIP-PROJECT/PSNR.m
+++ DIP-PROJECT/PSNR.m
@@ -6,17 +6,9 @@ function [ output ] = PSNR(input_img1, input_img2)
 % By default : gray level : 256, which indicate that [0 255]
 MAX1 = 255;
 % pass the check of legal of input_img
-if (rank(input_img1) == 3)
-    % rgb
-    % translate to ycbcr
-    input_img1 = rgb2ycbcr(input_img1);
-    input_img2 = rgb2ycbcr(input_img2);
-    % only use Y
-    input_img1 = input_img1(:, :, 1);
-    input_img2 = input_img2(:, :, 1);
-end
+
 [M, N] = size(input_img1);
-MSE = sum((input_img1 - input_img2) .^ 2) / (M * N);
+MSE = sum(sum((input_img1 - input_img2) .^ 2)) / (M * N);
 output = 20 * log10(MAX1 / (MSE ^ 0.5));
 
 end

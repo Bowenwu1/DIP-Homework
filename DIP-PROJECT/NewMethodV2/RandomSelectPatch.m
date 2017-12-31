403255ccd8640327c4ff4670fa49ec9f09ee51a3
Bob Wu <bowen.wu@163.com>
2017-12-31 10:29:44 +0800

    修改随机数的生成方式

--- DIP-PROJECT/NewMethodV2/RandomSelectPatch.m
+++ DIP-PROJECT/NewMethodV2/RandomSelectPatch.m
@@ -9,13 +9,13 @@ fprintf('INFO : start randomly select patch for kmeans\n');
 load('total_patch_num.mat');
 % load('LR_patch_set.mat');
 % load('HR_patch_set.mat');
-% seed = RandStream('mcg16807','Seed',0);
-% RandStream.setGlobalStream(seed) 
-% random_index = rand(num_patch_to_train_cluster_center,1);
-% random_index = ceil(random_index * total_patch_num);
-% random_index = sort(random_index,'ascend');
-random_index = randi([1 total_patch_num], 1, num_patch_to_train_cluster_center);
+seed = RandStream('mcg16807','Seed',0);
+RandStream.setGlobalStream(seed) 
+random_index = rand(num_patch_to_train_cluster_center,1);
+random_index = ceil(random_index * total_patch_num);
 random_index = sort(random_index,'ascend');
+% random_index = randi([1 total_patch_num], 1, num_patch_to_train_cluster_center);
+% random_index = sort(random_index,'ascend');
 load('LR_patch_set.mat');
 random_LR_patch_set = LR_patch_set(:, random_index);
 save('random_LR_patch_set.mat', 'random_LR_patch_set', '-v7.3');

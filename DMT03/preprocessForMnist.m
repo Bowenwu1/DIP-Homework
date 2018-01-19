c63f0ff3fe8089eccc47108436ce1b86524c0d63
Bob Wu <bowen.wu@163.com>
2018-01-19 13:42:09 +0800

    for some command

--- DMT03/preprocessForMnist.m
+++ DMT03/preprocessForMnist.m
@@ -4,10 +4,13 @@ function [ output, originOutput ] = preprocessForMnist( input )
 % first step : make it 2-value
 input = rgb2gray(input);
 input = im2bw(input, graythresh(input));
-% second step : make it square
+input = ~input;
+% second step : rotate the 2-value image
+input = rotateDigit(input);
+% third step : make it square
 [m, n] = size(input);
 outputSize = max([m, n]);
-output = ones(outputSize, outputSize);
+output = zeros(outputSize, outputSize);
 if m >= n
     aliasing = round((outputSize - n) / 2);
     output(1:outputSize, 1 + aliasing + 1 : 1 + aliasing + n) = input(:, :);
@@ -15,12 +18,11 @@ else
     aliasing = floor((outputSize - m) / 2);
     output(1 + aliasing : 1 + aliasing + m, 1 : outputSize) = input(:, :);
 end
-% third step : downsampling to 28 * 28
+% fourth step : downsampling to 28 * 28
 output_temp = imresize(output, [20 20]);
-output = ones(28, 28);
+output = zeros(28, 28);
 output(5:24, 5:24) = output_temp(:, :);
 output = im2bw(output, graythresh(output));
-output = ~output;
 output = double(output);
 originOutput = output;
 output = reshape(output, 1, 28 * 28);
---
+++ DMT03/rotateDigit.m
@@ -0,0 +1,20 @@
+function [ output ] = rotateDigit( input )
+% input : 2-value digit image(logical)
+% output : 2-value digit image(logical) and rotate
+% make the width of digit minimum
+    function [width] = widthOfROI(input)
+        roi = bwlabel(~input);
+        [~, col] = find(roi == 1);
+        width = max(col) - min(col)
+    end
+% assumption : -30 ~ +30
+degreeAliasing = 31;
+width = zeros(61, 1);
+for i = 1 : 61
+    width(i) = widthOfROI(imrotate(input, i - degreeAliasing));
+end
+[~, degree] = min(width)
+output = imrotate(input, degree - degreeAliasing);
+% imshow(output);
+end
+
